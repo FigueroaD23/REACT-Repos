bb2ec6e593b9e667426069d828761851bdf870cc
Angel Figueroa <angelnewrg4l@outlook.com>
2022-08-10 14:24:36 -0500

    gifs con service y keyword

--- gifs-context/src/App.jsx
+++ gifs-context/src/App.jsx
@@ -5,32 +5,35 @@ const gifsPrueba = ['https://media0.giphy.com/media/19JSJ5ucu91R5D7a3w/giphy-dow
 ]
 
 function App() {  
-  const [valor, setValor] = useState(0);
   const [gifs, setGifs] = useState(gifsPrueba);
-  const [linkAPI, setlinkAPI] = useState('https://api.giphy.com/v1/gifs/search?api_key=EoqFY62lYQlr3yurnxLFYxzHqvRZaWEF&q=goku&limit=25&offset=0&rating=g&lang=en');    
   const [keyword, setKeyWord] = useState('goku');
 
   useEffect(()=>{    
-    getGifsService(linkAPI).then((gifsAPI)=>{
+    getGifsService({keyword}).then((gifsAPI)=>{
       console.log(gifsAPI)
         setGifs([])
         setGifs(gifsAPI)
     })
-  },[linkAPI])
+  },[keyword])
 
   return (
-    <div className="container">
+    <>    
       <p>Hola mundo con React y Vite para practicar Context API y Router</p>
-    {valor}
+      <button onClick={()=>{setKeyWord("spongebob")}}>incrementar</button>
+    <div className="container">          
 
     {
-      gifs.map((item,index)=>{
+      gifs.map((item)=>{
         return(
-        <img width={60} height={40} src={item} alt={`gif de ${item}`} key={index} />)
+          <div className="gif">
+            <p>{item.title}</p>
+            <img width={120} height={80} src={item.url} alt={`gif de ${item}`} key={item.id} />
+          </div>
+        )
       })
     }
-    <button onClick={()=>{setlinkAPI("https://api.giphy.com/v1/gifs/search?api_key=EoqFY62lYQlr3yurnxLFYxzHqvRZaWEF&q=gears+of+war&limit=25&offset=0&rating=g&lang=en")}}>incrementar</button>
     </div>
+    </>
   )
 }
 
--- gifs-context/src/services/getGifsService.js
+++ gifs-context/src/services/getGifsService.js
@@ -1,9 +1,14 @@
-export default async function(linkAPI){
+const API_URL =  'https://api.giphy.com/v1/gifs/search?'
+const API_KEY =  'EoqFY62lYQlr3yurnxLFYxzHqvRZaWEF'
+export default async function({keyword = "morty"} = {}){    
+    const apiURL = `${API_URL}api_key=${API_KEY}&q=${keyword}&limit=30&offset=0&rating=g&lang=en`
     try {
-        const dataFirst = await fetch(linkAPI)
+        const dataFirst = await fetch(apiURL)
         const gifs = await dataFirst.json()
         const gifsAPI = gifs.data.map((gif) => {
-            return gif.images.fixed_width_downsampled.url
+            const {images,title,id} = gif
+            const {url} = gif.images.fixed_width_downsampled
+            return {url,images,title,id}
         })
         return gifsAPI
     } catch (e) {
