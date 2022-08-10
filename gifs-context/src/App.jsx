7f709f765ba9233758f9853b097c088f516aeec0
Angel Figueroa <angelnewrg4l@outlook.com>
2022-08-10 17:11:11 -0500

    Añadiendo router
    Router con link

--- gifs-context/src/App.jsx
+++ gifs-context/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react"
 import ListOfGifs from "./components/ListOfGifs";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+{/* <button onClick={()=>{setKeyWord("spongebob");console.log("hola")}}>incrementar</button>      */}
 
 function App() {    
   const [keyword, setKeyWord] = useState('goku'); 
   return (
-    <>           
-    <button onClick={()=>{setKeyWord("spongebob");console.log("hola")}}>incrementar</button>     
+    <Router>
       <div className="container">
-        <ListOfGifs keyword={keyword}/>        
-      </div>
-    </>
+        <nav>
+          <NavLink to="/gif/barbie">Barbie</NavLink>
+          <NavLink to="/gif/Gears of war">Gears of war</NavLink>
+          <NavLink to="/gif/"></NavLink>
+          <NavLink to="/gif/"></NavLink>
+          <NavLink to="/gif/"></NavLink>
+        </nav>
+        <hr />
+        <Switch>
+          <Route path="/" exact>HOME</Route>
+          <Route path="/gif/:keyword" component={ListOfGifs}/>
+        </Switch>
+      </div>    
+    </Router>         
   )
 }
 
--- gifs-context/src/components/ListOfGifs.jsx
+++ gifs-context/src/components/ListOfGifs.jsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import getGifsService from "../services/getGifsService";
 import Gif from "./Gif";
+import {useParams} from 'react-router-dom'
 
-const ListOfGifs = ({keyword}) => {     
+const ListOfGifs = (props) => {    
+    const {keyword} = useParams()    
+    //const {keyword} = props.match.params    
     const [gifs, setGifs] = useState([]);
     useEffect(()=>{    
         getGifsService({keyword}).then((gifsAPI)=>{
