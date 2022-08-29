925821f9a9ee39323655a41acc8fa9626e5e08af
Angel Figueroa <angelnewrg4l@outlook.com>
2022-08-29 12:38:27 -0500

    error cachado en el servicio gifEspecifico

--- gifs-context/src/services/getGifEspecifico.js
+++ gifs-context/src/services/getGifEspecifico.js
@@ -5,6 +5,10 @@ export default async function({id = ""} = {}){
     const apiURL = `${API_URL}${id}?api_key=${API_KEY}`
     try {
         const dataFirst = await fetch(apiURL)
+        console.log("PETICION STATUS",dataFirst.status)
+        if(dataFirst.status===404){
+            return{error:"No se encontró algún gif con ese ID"}
+        }
         const gifEspecifico = await dataFirst.json()
         const {images,title,id} = gifEspecifico.data
         const {url} = gifEspecifico.data.images.fixed_width_downsampled
