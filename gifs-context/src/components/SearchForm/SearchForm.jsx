5bcccd022941d0a061927624a21830154bf78419
Angel Figueroa <angelnewrg4l@outlook.com>
2022-09-05 09:10:11 -0500

    reducer con objeto de actions(sin switch)

--- gifs-context/src/components/SearchForm/SearchForm.jsx
+++ gifs-context/src/components/SearchForm/SearchForm.jsx
@@ -1,47 +1,76 @@
 import { useState, memo, useReducer} from "react";
 import { useHistory } from "react-router-dom";
+
+/* LOGICA DEL REDUCER */
 const ACTIONS = {
-    UPDATE_KEYWORD:'actualizarKeyWordAndTimes'
+    UPDATE_KEYWORD:'actualizarKeyWordAndTimes',
+    UPDATE_RATING:'actualizarRating'
 }
 const RATINGS = ["g","pg","pg-13","r"]
-
-const reducer = (state,{type,payload})=>{
+// Este es el switchh para el reducer de toda la vida
+/* const reducer = (state,{type,payload})=>{
     switch (type) {
         case ACTIONS.UPDATE_KEYWORD:
             return {...state,
                 keyword:payload.keyword,
                 times:state.times + 1
-            }                
+            } 
+        case ACTIONS.UPDATE_RATING:
+            return{...state,
+                rating:payload.rating                
+            }        
         default:
             return state
     }
-
+} */
+// Se puede poner también dela siguiente forma:
+// Se crea un objeto con lo que va a regresar cada caso de las actions
+const ACTIONS_REDUCERS = {
+    [ACTIONS.UPDATE_KEYWORD]: (state, payload) =>{
+        return {...state,
+            keyword:payload.keyword,
+            times:state.times + 1
+        }    
+    },
+    [ACTIONS.UPDATE_RATING]: (state, payload) =>{
+        return{...state,
+            rating:payload.rating
+        }
+    }
 }
+ const reducer = (state,{type,payload})=>{
+    const actionReducer = ACTIONS_REDUCERS[type]
+    return actionReducer ? actionReducer(state,payload) : state
+ }
+
+/* LOGICA DEL REDUCER */
 
 const SearchForm = ({initialKeyword=''}) => {    
     const initialState = {
         keyword:initialKeyword,
-        times:0
+        times:0,
+        rating:'g'
     }
     /* const [searchKey, setSearchKey] = useState(initialKeyword)
     const [times, setTimes] = useState(0) */
-    const [rating, setRating] = useState(RATINGS[0])
+    // const [rating, setRating] = useState(RATINGS[0])
 
     const [state, dispatch] = useReducer(reducer, initialState)
-    const {keyword:searchKeyword,times} = state
+    const {keyword:searchKeyword,times,rating} = state
     const history = useHistory()
-    const handleSubmit = (e)=>{          
+    const handleSubmit = (e)=>{
         e.preventDefault()
         //navegar a otra ruta
         history.push(`/search/${searchKeyword}/${rating}`)
     }
     const handleInputChange = (e)=>{   
-        dispatch({type:ACTIONS.UPDATE_KEYWORD,payload:{keyword:e.target.value}})       
+        dispatch({type:ACTIONS.UPDATE_KEYWORD,payload:{keyword:e.target.value}})
         /* setSearchKey(e.target.value)
         setTimes(prev=>prev+1) */
     }        
-    const handleSelectRating = (e)=>{   
-        setRating(e.target.value)
+    const handleSelectRating = (e)=>{
+        // setRating(e.target.value)
+        dispatch({type:ACTIONS.UPDATE_RATING,payload:{rating:e.target.value}})
     }        
 
     return (
